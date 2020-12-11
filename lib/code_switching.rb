e3daab4614851f68f19870d40315abb8dae4506d
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-12-10 23:10:24 -0600

    commit tree d1c97ac88b8d79f119f684ac8e47d7969dc8054d

---
+++ lib/code_switching.rb
@@ -0,0 +1,1 @@
+((@lengths[0] > 0) && (@lengths[1] > 0) && (@lengths[2] > 0)) && ((@lengths[0] + @lengths[1] > @lengths[2]) && (@lengths[0] + @lengths[2] > @lengths[1]) && (@lengths[1] + @lengths[2] > @lengths[0]))
